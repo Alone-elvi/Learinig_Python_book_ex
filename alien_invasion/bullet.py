59e1d042a40b218b9f1e632cd6f6735c082e92b6
alone.elvi@gmail.com <QMqs2MzvsnAjHhwFY2wuh>
2017-12-04 02:05:00 +0300

    Game Bullet

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -19,6 +19,11 @@ class Bullet(Sprite):
         self.rect.top = ship.rect.top
         self.bullets_allowed = 3
 
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
     def update(self):
         """Перемещает пулю вверх по экрану"""
         # Обновление позиции пули в вещественном формате.
